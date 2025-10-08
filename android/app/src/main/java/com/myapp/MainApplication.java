69a339875b1e0ce5a154e0727868a7daf82f853e
monica.vishwanath <monica.vishwanath@estuate.com>
2025-10-08 12:14:04 +0530

    gradlew clean worked

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -1,34 +1,40 @@
 package com.myapp;
 
 import android.app.Application;
-
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-
-import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
         }
 
         @Override
-        protected List<ReactPackage> getPackages() {
-          // Autolinking handles packages automatically now.
-          // If you have manual packages, add them here.
-          return Collections.emptyList();
+        protected List getPackages() {
+          return new ArrayList<>();
         }
 
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected boolean isHermesEnabled() {
+          return BuildConfig.IS_HERMES_ENABLED;
+        }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
         }
       };
 
@@ -40,6 +46,10 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, false);
+
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load();
+    }
   }
 }
